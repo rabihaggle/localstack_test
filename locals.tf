fd32e865d9b4deba374d101faa71934fe6bba19a
Nicolas Abihaggle <rabihaggle@gmail.com>
2023-08-31 21:01:24 +0200

    add networks

--- locals.tf
+++ locals.tf
@@ -1,13 +1,13 @@
 ## -> https://medium.com/@smburrows/terraform-workspace-variables-497535bf645e
 locals {
-    workspace_path = "./workspaces/${terraform.workspace}.yaml" 
-    defaults       = file("./workspaces/default.yaml")
-    workspace = fileexists(local.workspace_path) ? file(local.workspace_path) : yamlencode({})
-    settings = merge(
-        yamldecode(local.defaults),
-        yamldecode(local.workspace)
-    )
+  workspace_path = "./workspaces/${terraform.workspace}.yaml"
+  defaults       = file("./workspaces/default.yaml")
+  workspace      = fileexists(local.workspace_path) ? file(local.workspace_path) : yamlencode({})
+  settings = merge(
+    yamldecode(local.defaults),
+    yamldecode(local.workspace)
+  )
 }
 output "project_settings" {
-    value = local.settings
+  value = local.settings
 }
--- networking.tf
+++ networking.tf
@@ -1,4 +1,15 @@
 # Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = local.settings.vpc.cidr_block
+
+module "vpc" {
+  version                = "~>5.1.1"
+  source                 = "terraform-aws-modules/vpc/aws"
+  name                   = "${local.settings.team}-${local.settings.environment}"
+  cidr                   = local.settings.vpc.cidr_block
+  azs                    = data.aws_availability_zones.this.names
+  private_subnets        = local.settings.vpc.private_subnet_cidrs
+  public_subnets         = local.settings.vpc.public_subnet_cidrs
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = false
+  enable_dns_hostnames   = true
 }
