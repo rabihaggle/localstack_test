8d98bad08eec38e299ccb43f2bd57394f2dca256
Nicolas Abihaggle <rabihaggle@gmail.com>
2023-08-31 18:06:58 +0200

    .

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region                      = "eu-west-1"
   access_key                  = "test"
   secret_key                  = "test"
-  s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
